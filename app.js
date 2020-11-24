c9e13b5985715fe01d96672374838500edadc0ad
shotYazaki <69493228+shotYazaki@users.noreply.github.com>
2020-11-24 17:05:05 +0900

    1-create-register-page

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
-const  express = require('express');
+const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.render('index.ejs',　{title: 'Soccer world cup'});
+app.use(express.static('public'));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Ntpw0601',
+  detabase: 'test'
 });
 
-app.get('/login.ejs', (req, res) => {
-    res.render('login.ejs');
+
+
+
+app.get('/', (req, res) => {
+  res.render('top.ejs',　{title: 'Soccer world cup'});
 });
+
+
+
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs');
+});
+
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM boards',
+    (error, results) => {
+      res.render('index.ejs', {boards: results});
+    }
+  );
+});
+
+app.post('/create/:user_id', (req, res) => {
+  connection.query(
+    'INSERT INTO users (email) VALUES (?)',
+    [req.body.user_id],
+    (error, result) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+
 
 app.listen(3000);
